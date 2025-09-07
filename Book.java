ae7b2aaa1c9a9fcdc217f22fb4857def2d2bca9c
Noah Maitê / Nayara Pereira <168305331+NoahMaite@users.noreply.github.com>
2025-09-07 19:36:21 -0300

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,62 @@
+public class Book {
+    private String name;
+    private Author[] authors;
+    private double price;
+    private int qty = 0;
+
+    public Book(String name, Author[] authors, double price) {
+        this.name = name;
+        this.authors = authors;
+        this.price = price;
+    }
+
+    public Book(String name, Author[] authors, double price, int qty) {
+        this.name = name;
+        this.authors = authors;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author[] getAuthors() {
+        return authors;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public String getAuthorNames() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < authors.length; i++) {
+            sb.append(authors[i].getName());
+            if (i < authors.length - 1) sb.append(", ");
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(name + " by ");
+        for (int i = 0; i < authors.length; i++) {
+            sb.append(authors[i].toString());
+            if (i < authors.length - 1) sb.append(", ");
+        }
+        return sb.toString();
+    }
+}
